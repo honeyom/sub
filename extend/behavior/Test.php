de49935d705e46326b12b05bf91a58239f017a4c
root <d536819@163.com>
2020-07-05 16:54:08 +0800

    新增 使用控制器前后置中间件，绑定行为钩子。触发后续动作

---
+++ extend/behavior/Test.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *  文件：Test.php
+ *  author:daixianhua
+ *  时间：2020/7/5-15:34
+ */
+namespace behavior;
+
+use think\facade\Log;
+use think\facade\Request;
+
+class Test
+{
+
+//    public function appInit($params){
+//        \think\facade\Log::write('[DEBUG]'.date('Y:m:d H:i:s',time()).'写入了日志::'.Request::controller().'/'.Request::action());
+//    }
+    public function start(){
+        Log::debug(date('Y:m:d H:i:s',time()).'写入了日志::'.Request::controller().'/'.Request::action());
+    }
+}
